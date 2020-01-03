ea7cf316199370f56fd667f71afa72d435d84d39
Lucas Trigueiro <lucashtrigueiro@gmail.com>
2020-01-03 12:12:07 -0300

    Atualização: utilizando somente JsonIgnore

--- src/main/java/com/lucas/cursomc/domain/Pedido.java
+++ src/main/java/com/lucas/cursomc/domain/Pedido.java
@@ -18,7 +18,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {
@@ -32,7 +31,6 @@ public class Pedido implements Serializable {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date instante;
 
-	@JsonManagedReference
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 	
@@ -40,7 +38,6 @@ public class Pedido implements Serializable {
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name="endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
