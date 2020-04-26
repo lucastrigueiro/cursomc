11a0003ae71fa4a70888d6ac5354060926598e16
lucas <lucas.trigueiro@monitoratec.com.br>
2020-04-26 15:39:33 -0300

    Deletando uma categotia com DELETE

--- src/main/java/com/lucas/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/lucas/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -8,14 +8,23 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.lucas.cursomc.services.exceptions.ObjectNotFoundException;
+import com.lucas.cursomc.services.exceptions.DataIntegrityException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-		
+
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
 }
--- src/main/java/com/lucas/cursomc/services/CategoriaService.java
+++ src/main/java/com/lucas/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.lucas.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lucas.cursomc.domain.Categoria;
 import com.lucas.cursomc.repositories.CategoriaRepository;
+import com.lucas.cursomc.services.exceptions.DataIntegrityException;
 import com.lucas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -30,4 +32,15 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possua produtos");
+		}
+	}
+
+
 }
