eaffa9965d79eb65b551cf63a0ab724016262515
lucas <lucas.trigueiro@monitoratec.com.br>
2020-04-26 16:40:58 -0300

    Listando todas categorias com DTO

--- src/main/java/com/lucas/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/lucas/cursomc/resources/CategoriaResource.java
@@ -1,7 +1,10 @@
 package com.lucas.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.lucas.cursomc.dto.CategoriaDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,4 +50,12 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+
 }
