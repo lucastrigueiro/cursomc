39388b8869c6444e8872980ee6d9cc74c520f899
lucas <lucas.trigueiro@monitoratec.com.br>
2020-06-01 16:04:59 -0300

    MockEmailService com Logger. Padrões Strategy e Template Method

--- src/main/java/com/lucas/cursomc/config/TestConfig.java
+++ src/main/java/com/lucas/cursomc/config/TestConfig.java
@@ -1,6 +1,8 @@
 package com.lucas.cursomc.config;
 
 import com.lucas.cursomc.services.DBService;
+import com.lucas.cursomc.services.EmailService;
+import com.lucas.cursomc.services.MockEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,5 +21,10 @@ public class TestConfig {
     public boolean instantiateDatabase() throws ParseException {
         dbService.instantiateDatabase();
         return true;
+    }
+
+    @Bean
+    public EmailService emailService(){
+        return new MockEmailService();
     }
 }
